f1fb96e1fbb2aeb9728c6f71b2e525ba3b71be4b
Michał Grycki <mgrycki@michas-mbp.home>
2024-01-22 13:25:23 +0100

    update

--- js-course/05-Guess-My-Number/starter/script.js
+++ js-course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+// Handling Click Events
+
+// Defining secret number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  // We do conversion as this is String and we need Number
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // First scenario, checking if there's number
+  if (!guess) {
+    displayMessage('NO NUMBER');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // #2 Highscore adding
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // Second scenario, is number is different
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!' : 'TOO LOW!');
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You Lost');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Challenge #1 Implement Reset Button Functionality
+document.querySelector('.again').addEventListener('click', function () {
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = null;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
